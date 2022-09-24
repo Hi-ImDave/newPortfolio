a4d7109d60d2eff59d7e44348fb8a82f5e442c0f
David Mott <d.s.mott0808@gmail.com>
2022-09-24 10:47:07 -0700

    responsive work

--- script.js
+++ script.js
@@ -7,10 +7,14 @@ const btn = document.getElementById('open-btn')
 const span = document.getElementsByClassName('close')[0]
 const labels = document.querySelectorAll('.form-control label')
 const panels = document.querySelectorAll('.panel')
+const slides = document.getElementsByClassName('slides')
+const dots = document.getElementsByClassName('dot')
+const slidesArray = Array.from(slides)
 
 open.addEventListener('click', () => container.classList.add('show-nav'))
 close.addEventListener('click', () => container.classList.remove('show-nav'))
 navClose.addEventListener('click', () => container.classList.remove('show-nav'))
+btn.addEventListener('click', () => container.classList.remove('show-nav'))
 
 panels.forEach((panel) => {
   panel.addEventListener('click', () => {
@@ -36,7 +40,7 @@ labels.forEach((label) => {
 })
 
 btn.onclick = () => {
-  modal.style.display = 'flex'
+  setTimeout((modal.style.display = 'flex'), 500)
 }
 
 span.onclick = () => {
@@ -47,4 +51,39 @@ window.onclick = (event) => {
   if (event.target == modal) {
     modal.style.display = 'none'
   }
+}
+
+// SLIDES
+
+// ADD REMOVE CLASS BASED ON SCREEN SIZE / ORIENTATION
+
+let slideIndex = 1
+showSlides(slideIndex)
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n))
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n))
+}
+
+function showSlides(n) {
+  let i
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none'
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active', '')
+  }
+  slides[slideIndex - 1].style.display = 'block'
+  dots[slideIndex - 1].className += ' active'
 }
